f384b033a460f3828ffdad02ee4acbed3d3af512
mayuri-raut <rautmayuri204@gmail.com>
2019-07-30 18:05:58 +0530

    files added

--- bankenfe/src/App.js
+++ bankenfe/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import Details from  '../src/admin/components/Employee/Details.jsx';
+import Details from  './admin/components/EmployeeDetails/Details';
 import Login from './com/components/Login/Login';
 import Signup from './com/components/SignUp/Signup';
+import Userflow from './workflow/components/Userflow/Userflow';
+
 
 function App() {
   return (
     <div>
       {/* <Login></Login> */}
-      <Signup></Signup>
+      {/* <Signup></Signup> */}
      {/* <Details></Details> */}
+      <Userflow></Userflow>
     </div>
   );
 }
