733b20746d63580a070ab0d38f27dd333a46cf15
madhuriDesai <madhuri@bakedmoon.studio>
2019-08-22 18:52:26 +0530

    WIP app

--- gameApp/App.js
+++ gameApp/App.js
@@ -1,54 +1,89 @@
-import React, { Component } from 'react';
- 
-import { StyleSheet, View, Button, Text } from 'react-native';
- 
+import React, { Component } from "react";
+
+import { View, StyleSheet, Text } from "react-native";
+
 export default class App extends Component {
- 
-  constructor(){
- 
-    super();
- 
-    this.state={
- 
-      // This is our Default number value
-      NumberHolder : 1
- 
-    }
-  }
- 
-GenerateRandomNumber=()=>
-{
- 
-var RandomNumber = Math.floor(Math.random() * 100) + 1 ;
- 
-this.setState({
- 
-  NumberHolder : RandomNumber
- 
-})
-}
- 
   render() {
     return (
-   
-      <View style={styles.MainContainer} >
- 
-       <Text style={{marginBottom: 10, fontSize: 20}}>{this.state.NumberHolder}</Text>
- 
-       <Button title="Generate Random Number" onPress={this.GenerateRandomNumber} />
-        
+      // Try setting `flexDirection` to `column`.
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "flex-start"
+        }}
+      >
+        <View
+          style={{
+            paddingTop: 20,
+            paddingBottom: 20,
+            backgroundColor: "yellow"
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              borderColor: "red",
+              borderWidth: 1
+            }}
+          >
+            <Text style={{ flex: 1, borderColor: "red", borderWidth: 1 }}>
+              Left with long sd asdf text
+            </Text>
+            <Text
+              style={{
+                flex: 1,
+                borderColor: "red",
+                borderWidth: 1,
+                textAlign: "center"
+              }}
+            >
+              Title
+            </Text>
+            <Text
+              style={{
+                flex: 1,
+                borderColor: "red",
+                borderWidth: 1,
+                textAlign: "right"
+              }}
+            >
+              Right
+            </Text>
+          </View>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.card} />
+          <View style={styles.card} />
+          <View style={styles.card} />
+          <View style={styles.card} />
+        </View>
+        <View style={styles.container}>
+          <View style={styles.card} />
+          <View style={styles.card} />
+          <View style={styles.card} />
+          <View style={styles.card} />
+        </View>
       </View>
- 
     );
   }
 }
- 
-const styles = StyleSheet.create(
-{
-  MainContainer: {
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "green",
+    borderColor: "red",
+    borderWidth: 1
+  },
+  card: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: 80,
+    backgroundColor: "#000",
+    margin: 5,
+    shadowOffset: { width: 10, height: 10 },
+    shadowColor: "black",
+    shadowOpacity: 1.0
   }
- 
 });
